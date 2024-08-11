4214d4caccf505450aa840d8571c00c245cccb3d
Melvin <melvinordonez40@gmail.com>
2024-08-10 22:46:32 -0600

    first commit

---
+++ src/main/java/com/mycompany/aplicacionbancaria/AplicacionBancaria.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.aplicacionbancaria;
+
+/**
+ *
+ * @author melvin
+ */
+public class AplicacionBancaria {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
