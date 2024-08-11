299874593830a9994645fecf034bb6e80a884abb
Melvin <melvinordonez40@gmail.com>
2024-08-11 00:28:17 -0600

    Interfaz1

--- src/main/java/com/mycompany/aplicacionbancaria/AplicacionBancaria.java
+++ src/main/java/com/mycompany/aplicacionbancaria/AplicacionBancaria.java
@@ -1,7 +1,6 @@
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  */
-
 package com.mycompany.aplicacionbancaria;
 
 /**
@@ -11,6 +10,8 @@ package com.mycompany.aplicacionbancaria;
 public class AplicacionBancaria {
 
     public static void main(String[] args) {
+
         System.out.println("Hello World!");
+
     }
 }
